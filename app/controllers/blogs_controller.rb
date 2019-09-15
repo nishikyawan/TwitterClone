2468f21b5dc24a46546916d0100471c8ecd4353a
shinji nishikawa <rxycnp@yahoo.co.jp>
2019-09-16 01:34:36 +0900

    課題提出

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,50 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:edit, :update, :destroy]
+
+  def index
+  end
+
+  def new
+    @blog = Blog.new
+    @blogs = Blog.all.order(id: "DESC")
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to new_blog_path, notice: "ツイートしました！"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @content.update(blog_params)
+      redirect_to new_blog_path, notice: "ツイートを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @content.destroy
+    redirect_to new_blog_path
+  end
+
+  def confirm
+    @blog = Blog.new(blog_params)
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:content, :created_at, :updated_at)
+  end
+
+  def set_blog
+    @content = Blog.find(params[:id])
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,11 @@
+ActiveRecord::Schema.define(version: 2019_09_12_104025) do
+
+  enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
